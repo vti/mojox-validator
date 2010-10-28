dbfd12982cf5d14005f63f60fb2eca15498c9227
vti <viacheslav.t@gmail.com>
2010-10-29 00:00:42 +0200

    Made create_validator helper smarter

--- lib/Mojolicious/Plugin/Validator.pm
+++ lib/Mojolicious/Plugin/Validator.pm
@@ -5,16 +5,45 @@ use warnings;
 
 use base 'Mojolicious::Plugin';
 
+use Mojo::ByteStream;
+use Mojo::Loader;
 use MojoX::Validator;
+
+require Carp;
 
 sub register {
     my ($self, $app, $conf) = @_;
 
     $conf ||= {};
-    $conf->{messages} ||= {};
 
-    $app->helper(create_validator =>
-          sub { MojoX::Validator->new(messages => $conf->{messages}) });
+    $app->helper(
+        create_validator => sub {
+            my $self       = shift;
+            my $class_name = shift;
+
+            $class_name ||= 'MojoX::Validator';
+
+            unless ($class_name =~ m/[A-Z]/) {
+                my $namespace = ref($self->app) . '::';
+                $namespace = '' if $namespace =~ m/^Mojolicious::Lite/;
+
+                $class_name = join '' => $namespace,
+                  Mojo::ByteStream->new($class_name)->camelize;
+            }
+
+            my $e = Mojo::Loader->new->load($class_name);
+
+            Carp::croak qq/Can't load validator '$class_name': / . $e->message
+              if ref $e;
+
+            Carp::croak qq/Can't find validator '$class_name'/ if $e;
+
+            Carp::croak qq/Wrong validator '$class_name' isa/
+              unless $class_name->isa('MojoX::Validator');
+
+            return $class_name->new(%$conf, @_);
+        }
+    );
 
     $app->helper(
         validate => sub {
@@ -83,9 +112,9 @@ Mojolicious::Plugin::Validator - Plugin for MojoX::Validator
 
     @@ user.html.ep
     %= form_for 'user' => begin
-        <%= label 'username' => begin %>Username<% end %>
-        <%= input 'username' %>
-        <%= validator_error 'username' %>
+        <label for="username">Username</label><br />
+        <%= input_tag 'username' %><br />
+        <%= validator_error 'username' %><br />
 
         <%= submit_button %>
     % end
@@ -114,6 +143,29 @@ Replace default errors.
 =back
 
 =head2 Helpers
+
+=over
+
+    my $validator = $self->create_validator;
+    $validator->field('username')->required(1)->length(3, 20);
+
+Create L<MojoX::Validator>.
+
+    $self->create_validator('will-be_decamelized');
+    $self->create_validator('Custom::Class');
+
+Create a validator from a class derived from L<MojoX::Validator>. This way
+preconfigured validators can be used.
+
+=back
+
+=over
+
+    $self->validator($validator);
+
+Validate parameters with provided validator and automatically set errors.
+
+=back
 
 =over
 
--- t/plugin.t
+++ t/plugin.t
@@ -1,5 +1,34 @@
 #!/usr/bin/env perl
 
+package Custom;
+
+use base 'MojoX::Validator';
+
+sub new {
+    my $self = shift->SUPER::new(@_);
+
+    $self->field('foo')->required(1)->length(3, 10);
+
+    return $self;
+}
+
+package Decamelized;
+use base 'Custom';
+
+package WrongIsa;
+
+sub new { }
+
+package Broken;
+
+use base 'MojoX::Validator';
+
+sub new {
+    die 'broken!';
+}
+
+package main;
+
 use strict;
 use warnings;
 
@@ -7,15 +36,13 @@ use Test::More;
 
 plan skip_all => 'working sockets required for this test!'
   unless Mojo::IOLoop->new->generate_port;
-plan tests => 9;
+plan tests => 31;
 
 use Mojolicious::Lite;
 
-app->log->level('fatal');
-
 plugin 'validator';
 
-post '/' => sub { 
+post '/' => sub {
     my $self = shift;
 
     my $validator = $self->create_validator;
@@ -27,15 +54,59 @@ post '/' => sub {
     }
 } => 'form';
 
+post '/custom' => sub {
+    my $self = shift;
+
+    my $validator = $self->create_validator('Custom');
+
+    if ($self->validate($validator)) {
+        $self->render('ok');
+    }
+
+    $self->render('form');
+} => 'custom';
+
+post '/decamelized' => sub {
+    my $self = shift;
+
+    my $validator = $self->create_validator('decamelized');
+
+    if ($self->validate($validator)) {
+        $self->render('ok');
+    }
+
+    $self->render('form');
+} => 'decamelized';
+
+post '/broken' => sub {
+    my $self = shift;
+
+    $self->create_validator('Broken');
+} => 'broken';
+
+post '/wrong-isa' => sub {
+    my $self = shift;
+
+    $self->create_validator('WrongIsa');
+} => 'wrong-isa';
+
 use Test::Mojo;
 
 my $t = Test::Mojo->new;
 
-$t->post_form_ok('/' => {})->status_is(200)->content_like(qr/required/i);
+#$ENV{MOJO_LOG_LEVEL} = 'debug';
 
-$t->post_form_ok('/' => {foo => '12345678901'})->status_is(200)->content_like(qr/length/i);
+for my $path ('', 'custom', 'decamelized') {
+    $t->post_form_ok("/$path" => {})->status_is(200)
+      ->content_like(qr/required/i);
+    $t->post_form_ok("/$path" => {foo => '12345678901'})->status_is(200)
+      ->content_like(qr/length/i);
+    $t->post_form_ok("/$path" => {foo => '123'})->status_is(200)
+      ->content_like(qr/ok/i);
+}
 
-$t->post_form_ok('/' => {foo => '123'})->status_is(200)->content_like(qr/ok/i);
+$t->post_form_ok('/wrong-isa' => {})->status_is(500);
+$t->post_form_ok('/broken'    => {})->status_is(500);
 
 __DATA__
 
